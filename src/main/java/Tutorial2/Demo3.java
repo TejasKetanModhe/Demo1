9fcf960bf86a67298a1da103dd425f9e8df76509
sic <sic@DESKTOP-9KGJ58D>
2022-12-16 15:16:50 +0530

    Kahitari

---
+++ src/main/java/Tutorial2/Demo3.java
@@ -0,0 +1,14 @@
+package Tutorial2;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		String a="Ketan";
+		String b="Tejas";
+		String c=b+a;
+		System.out.println(c);
+			
+
+	}
+
+}
